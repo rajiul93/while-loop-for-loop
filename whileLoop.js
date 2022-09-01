e4cb9612cd27ad67debfab7bbc4eea3761726048
rajiul93 <rajiulrayhan@gmail.com>
2022-09-01 16:38:41 +0600

    just a simple idea for'while loop and for loop'

---
+++ whileLoop.js
@@ -0,0 +1,18 @@
+var cars = ["BMW", "Volvo", "Saab", "Tata"];
+for(var i = 0; i<cars.length; i++ ){
+    var element = cars[i];
+    console.log(element);
+}
+
+var friendName =['Hasan', 'Asek', 'Selim', 'Kolim'];
+for(var i = 0; i<friendName.length; i++){
+    var Names = friendName[i];
+    console.log('your friends answer:',Names);
+}
+
+var inputValue = 0;
+// while loop condition 
+while (inputValue <=10){
+    console.log(inputValue);
+    inputValue++;
+}
